1ca311f710302c31890f026dec4a6c3245e1f78b
Daniel Mikusa <dmikusa@gopivotal.com>
2013-08-19 12:22:07 -0400

    Refactored RandomDataNbioServlet   - Split read and write listeners into separate private classes   - Added some data checks to make sure input is valid   - Switched random output to be random alpha / numeric chars

--- src/main/java/com/pivotal/demos/nbio/RandomDataNbioServlet.java
+++ src/main/java/com/pivotal/demos/nbio/RandomDataNbioServlet.java
@@ -17,84 +17,95 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 @WebServlet(urlPatterns="/non-blocking-io/RandomDataNbioServlet", asyncSupported=true)
 public class RandomDataNbioServlet extends HttpServlet  {
-	
+
 	private static final long serialVersionUID = -6167956299941229517L;
+
+	private static class NumberReadListener implements ReadListener {
+		private StringBuilder buf = new StringBuilder();  // buffer for user's data
+		private AsyncContext context;
+
+		public NumberReadListener(AsyncContext context) {
+			this.context = context;
+		}
+
+		public void onDataAvailable() throws IOException {
+			// 3. Read in data from user, this should be a number representing the amount
+			//    of random characters to write back to the user
+			ServletInputStream input = context.getRequest().getInputStream();
+			try {
+	            byte[] b = new byte[8192];
+	            int read = 0;
+	            do {
+	                read = input.read(b);
+	                if (read == -1) {
+	                    break;
+	                }
+	                buf.append(new String(b, 0, read));
+	            } while (input.isReady());
+	        } catch (Exception ex) {
+	            ex.printStackTrace(System.err);
+	            context.complete();
+	        }
+		}
+
+		public void onAllDataRead() throws IOException {
+			// 4. Try to convert the string data into a number.  Defaults to 512.  Max 1,000,000.
+			int randomDataSize = 512;
+			if (buf.length() > 0) {
+				try {
+					randomDataSize = Integer.parseInt(buf.toString());
+				} catch (NumberFormatException ex) {
+					System.err.println("Invalid size specified, using default size of 512 chars");
+				}
+			}
+			randomDataSize = (randomDataSize < 0) ? 512 : randomDataSize;  // must be >= 0
+			randomDataSize = (randomDataSize > 1_000_000) ? 1_000_000 : randomDataSize; // must be <= 1,000,000
+
+			// 5. Configure a write listener to write the random characters
+			context.getResponse().getOutputStream().setWriteListener(
+								new RandomDataWriteListener(context, randomDataSize));
+		}
+
+		public void onError(Throwable throwable) {
+			throwable.printStackTrace(System.err);
+			context.complete();
+		}
+	}
+
+	private static class RandomDataWriteListener implements WriteListener {
+		private AsyncContext context;
+		private int numChars;
+
+		public RandomDataWriteListener(AsyncContext context, int numChars) {
+			this.context = context;
+			this.numChars = numChars;
+		}
+
+		public void onWritePossible() throws IOException {
+			ServletOutputStream output = context.getResponse().getOutputStream();
+			// 6. Write random characters
+			if (output.isReady()) {
+				output.write(RandomStringUtils.randomAlphanumeric(numChars).getBytes("utf-8"));
+			}
+
+			// 7. Call complete, to signal we are done
+			context.complete();
+		}
+
+		public void onError(Throwable throwable) {
+			throwable.printStackTrace(System.err);
+			context.complete();
+		}
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 1. Start Async
-		final AsyncContext asyncContext = req.startAsync();
-		final ServletInputStream servletInputStream = asyncContext.getRequest().getInputStream();
+		final AsyncContext context = req.startAsync();
 		resp.setCharacterEncoding("utf-8");
-		
-		// 2. Add Read Listener to get user's input
-		ReadListener listener = new ReadListener() {
-			
-			private StringBuilder sb = new StringBuilder();  // buffer user's request
-			
-			public void onDataAvailable() throws IOException {
-				// 4. Read all the data that is available, may be called multiple times
-				try {
-		            byte[] b = new byte[8192];
-		            int read = 0;
-		            do {
-		                read = servletInputStream.read(b);
-		                if (read == -1) {
-		                    break;
-		                }
-		                sb.append(new String(b, 0, read));
-		            } while (servletInputStream.isReady());
-		        } catch (Exception ex) {
-		            ex.printStackTrace(System.err);
-		            asyncContext.complete();
-		        }
-			}
-			
-			// 5. Called when all data has been read			
-			public void onAllDataRead() throws IOException {
-				System.out.println("onAllDataRead");
-				final ServletOutputStream outputStream = asyncContext.getResponse().getOutputStream();
-				
-				int randomDataSize = 512;
-				if (sb.length() > 0) {
-					try {
-						randomDataSize = Integer.parseInt(sb.toString());
-					} catch (NumberFormatException ex) {
-						// ignore, use default size
-					}
-				}
-				randomDataSize = (randomDataSize > 1_000_000) ? 1_000_000 : randomDataSize;
-				
-				final String data = RandomStringUtils.random(randomDataSize);
-				
-				// 6. Configure a write listener to echo the response
-				WriteListener listener = new WriteListener() {
-					public void onWritePossible() throws IOException {
-						// 7. Write output
-						if (outputStream.isReady()) {
-							outputStream.write(data.getBytes("utf-8"));
-						}
-						
-						// 8. Call complete, to signal we are done
-						asyncContext.complete();
-					}
-					
-					public void onError(Throwable throwable) {
-						throwable.printStackTrace(System.err);
-						asyncContext.complete();
-					}
-				};
-				outputStream.setWriteListener(listener);
-			}
-			
-			public void onError(Throwable throwable) {
-				throwable.printStackTrace(System.err);
-				asyncContext.complete();
-			}
-		};
-		
-		// 3. Add listener, starts Non-blocking IO support
-		servletInputStream.setReadListener(listener);
+
+		// 2. Add Read Listener to get the number of characters to return
+		req.getInputStream().setReadListener(new NumberReadListener(context));
 	}
-	
+
 }
