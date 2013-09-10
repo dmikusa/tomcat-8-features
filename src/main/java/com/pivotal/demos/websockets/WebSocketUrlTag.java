13b55c67d75f9930d8cd8f2c1df725bc81307681
Daniel Mikusa <dmikusa@gopivotal.com>
2013-09-10 10:08:10 -0400

    Added websocket tag lib for generating ws:// and wss:// urls

---
+++ src/main/java/com/pivotal/demos/websockets/WebSocketUrlTag.java
@@ -0,0 +1,44 @@
+package com.pivotal.demos.websockets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * Utility tag for creating websocket URLs in JSP pages.
+ */
+public class WebSocketUrlTag extends TagSupport {
+	private static final long serialVersionUID = 5870022969801469277L;
+
+	private String value;
+
+	@Override
+	public int doEndTag() throws JspException {
+		HttpServletRequest req = (HttpServletRequest) pageContext.getRequest();
+		StringBuilder sb = new StringBuilder();
+		sb.append("ws://");
+		sb.append(req.getServerName());
+		sb.append(":");
+		sb.append(req.getServerPort());
+		sb.append(req.getContextPath());
+		if (! value.startsWith("/")) {
+			sb.append("/");
+		}
+		sb.append(value);
+		try {
+			pageContext.getOut().print(sb.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return EVAL_PAGE;
+	}
+
+	public void setValue(String value) {
+		if (value == null) {
+			value = "/";
+		}
+		this.value = value;
+	}
+}
